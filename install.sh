ed70ff9fc4fe7a6d76964b3c654ed6ba35a1e3aa
Bob <olahbalazs@outlook.com>
2021-03-31 10:34:59 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-# Install Ansible and Ansible Galaxy roles on Ubuntu
-# Run playbook: ansible-playbook main.yml
-
-sudo apt-get -y update
-sudo apt-get -y install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get -y install ansible
-ansible-galaxy install -r requirements.yml
-
